91e55b479d7c65a8e8f4a34248530b2a28d0eee5
mivik <mivik@qq.com>
2022-12-10 22:03:03 +0800

    fix(render): fix hold note render issue

--- prpr/src/core/note.rs
+++ prpr/src/core/note.rs
@@ -137,7 +137,10 @@ fn draw_center(res: &Resource, tex: Texture2D, scale: f32, color: Color) {
 
 impl Note {
     pub fn plain(&self) -> bool {
-        !self.fake && self.speed == 1.0 && self.object.translation.1.keyframes.len() <= 1
+        !self.fake
+            && !matches!(self.kind, NoteKind::Hold { .. })
+            && self.speed == 1.0
+            && self.object.translation.1.keyframes.len() <= 1
     }
 
     pub fn update(&mut self, res: &mut Resource, object: &mut Object) {
