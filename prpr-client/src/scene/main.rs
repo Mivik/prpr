519918bbbd0b68de3133a1cc7b8914a03fe56432
mivik <mivik@qq.com>
2023-01-11 23:35:45 +0800

    chore: bump to 0.1.16

--- prpr-client/src/scene/main.rs
+++ prpr-client/src/scene/main.rs
@@ -356,17 +356,22 @@ impl MainScene {
     }
 
     fn render_about(ui: &mut Ui) {
-        const ABOUT: &str = r"prpr-client v0.1.13
+        static ABOUT: Lazy<String> = Lazy::new(|| {
+            format!(
+                r"prpr-client v{}
 prpr 是一款 Phigros 模拟器，旨在为自制谱游玩提供一个统一化的平台。请自觉遵守社群相关要求，不恶意使用 prpr，不随意制作或传播低质量作品。
 
 本软件使用的默认材质皮肤（包括音符材质和打击特效）来自于 @MisaLiu 的 phi-chart-render（https://github.com/MisaLiu/phi-chart-render），在 CC BY-NC 4.0 协议（https://creativecommons.org/licenses/by-nc/4.0/）下署名。在本软件的开发过程中，这些材质被调整尺寸并压缩以便使用。
 
 prpr 是开源软件，遵循 GNU General Public License v3.0 协议。
 测试群：660488396
-GitHub: https://github.com/Mivik/prpr";
+GitHub: https://github.com/Mivik/prpr",
+                env!("CARGO_PKG_VERSION")
+            )
+        });
         ui.dx(0.02);
         ui.dy(0.01);
-        ui.text(ABOUT).multiline().max_width((1. - SIDE_PADDING) * 2. - 0.02).size(0.5).draw();
+        ui.text(&*ABOUT).multiline().max_width((1. - SIDE_PADDING) * 2. - 0.02).size(0.5).draw();
     }
 
     fn render_account(ui: &mut Ui, page: &mut AccountPage) {
