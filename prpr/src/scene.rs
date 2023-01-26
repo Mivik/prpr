b274dd524da25a8aaace90d2175c2db960a87215
mivik <mivik@qq.com>
2023-01-26 09:26:19 +0800

    feat(client): add error message for iOS

--- prpr/src/scene.rs
+++ prpr/src/scene.rs
@@ -142,9 +142,14 @@ pub fn request_file(id: impl Into<String>) {
                     extern "C" fn document_picker(_: &Object, _: Sel, _: ObjcId, documents: ObjcId) {
                         unsafe {
                             let url: ObjcId = msg_send![documents, firstObject];
-                            let data: ObjcId = msg_send![class!(NSData), dataWithContentsOfURL: url];
+                            let mut error: ObjcId = std::ptr::null();
+                            let data: ObjcId = msg_send![class!(NSData), dataWithContentsOfURL: url options: 2 error: &mut error as *mut ObjcId];
                             if data.is_null() {
                                 show_message("读取文件失败");
+                                if !error.is_null() {
+                                    let msg: *const NSString = msg_send![error, localizedDescription];
+                                    show_error(Error::msg((*msg).as_str()).context("读取文件失败"));
+                                }
                             } else {
                                 extern "C" {
                                     #[allow(improper_ctypes)]
